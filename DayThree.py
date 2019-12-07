3803bd0ac712eca4141370a8d29fc5f98ee0d7d7
Charlie Parece <cparece22@bsmschool.org>
2019-12-06 20:23:47 -0600

    mmmmm

--- DayThree.py
+++ DayThree.py
@@ -1,16 +1,113 @@
-Grid = [None] * 10000
-pathOne = [R997,D543,L529,D916,R855,D705,L159,U444,R234,U639,L178,D682,L836,U333,R571,D906,L583,U872,L733,U815,L484,D641,R649,U378,L26,U66,L659,D27,R4,U325,L264,D711,L837,D986,L38,U623,L830,D369,L469,D704,L302,U143,L771,U170,R237,U477,L251,D100,R561,D889,R857,D780,R258,D299,L975,D481,L692,D894,R847,D416,R670,D658,L537,U748,R468,D304,L263,D884,R806,D13,R288,U933,R4,U291,L809,D242,R669,D50,R106,D510,R409,U311,R101,D232,R370,D490,L762,D805,L981,D637,L987,U403,R965,U724,L404,D664,L687,U868,L808,D174,L363,D241,L54,D238,R444,U75,R683,U712,L759,D569,R349,D378,L576,U437,R137,D822,R21,D595,L602,U147,R959,U350,R964,U625,L718,U331,L252,D386,L251,U371,R973,D709,R915,D837,L7,U727,L501,D520,L626,U161,L287,D224,L821,U555,L312,U234,L335,D572,L113,U673,L615,D919,R925,U16,R211,U77,R630,U786,R850,D221,R939,U559,R887,U779,L222,D482,L252,D682,L904,U568,R317,D453,R689,D917,R845,U260,R69,U613,R528,D447,L791,D119,L268,U215,L806,U786,R465,D787,L792,D823,R526,D709,L362,D748,L518,U115,L898,U784,R893,U911,R98,U215,R828,D100,R153,U496,L938,D403,R886,D317,L849,D59,R156,D27,L64,D771,R956,U880,R313,D244,L483,D17,R72,U467,L475,D444,R554,D781,R524,D152,L771,U435,L622,D601,R733,D478,L686,D12,L525,D467,L302,D948,L966,U572,L303,U914,R54,D417,R635,D425,R640,D703,R17,D187,L195,U59,R166,D616,L557,U458,L743,D166,R328,D640,R908,D775,L151,D216,L964,D202,L534,D239,R998,U167,L604,D812,L527,U526,L640,U93,L733,D980,R607,D879,L593,D721,R454,U137,R683,D343,L38,D398,L81,U392,R821,U247,L361,D208,R763,D771,L515]
-pathTwo = [L1000,D189,L867,U824,L193,D12,R704,U83,R371,D858,L970,D56,R877,D448,R962,U239,R641,D198,L840,D413,R586,D920,R650,U919,R375,D540,L150,U995,R54,D200,R61,D974,R249,U893,R319,U930,R658,U680,R286,D186,R963,U553,L256,U629,L554,U576,R887,U595,R629,D680,L684,U556,L302,U348,R825,D252,L684,U705,L258,D72,R907,U702,L518,U440,R239,U258,R825,U27,L580,D613,R357,D468,R519,U833,L415,D822,L798,U904,R812,U76,R86,U252,R427,U637,L896,U147,L294,U381,R306,U423,L688,D336,R648,U677,L750,D218,L649,D360,R710,D64,R317,U232,R261,D167,L49,D138,L431,D505,L535,U294,L553,U969,L144,U227,R437,D397,R359,U848,L48,D992,R169,D580,L219,D525,R552,U546,R849,D722,R894,D735,L182,U570,R274,D349,R312,U430,R441,U183,R645,D308,L416,U333,L687,U202,L973,D736,R382,U260,L176,D207,R706,U52,L142,D746,L328,D413,R879,D429,L679,D695,L224,D462,R358,D124,L515,D629,L873,D759,L763,U28,R765,D426,L93,U927,L395,U243,L393,D488,L729,U100,R488,D83,R47,U92,L871,D410,R405,D993,R537,D10,L79,D218,L686,D563,L31,U885,L784,D462,L160,U345,R204,U275,R162,U164,R843,D578,R255,D456,L398,U470,L576,D973,L337,D971,R205,U264,R707,U975,L60,U270,R1,U808,R844,D884,L952,D435,L144,D374,R389,D741,R404,D398,R282,D807,L316,U136,L504,U720,R859,D925,L711,U343,L535,D978,R578,U636,L447,D298,R574,U590,L142,D802,L846,D617,L838,U362,R812,U295,L328,U162,L617,D857,L759,D251,L343,U394,R721,U320,R836,U726,L950,D612,R129,U549,L970,D87,L341,D269,L659,U550,R835,D318,L189,U278,R871,D62,R703,U807,L389,U824,R521,D175,L698,U313,L942,D810,L498,U18,R168,D111,R607]
 
-def moveRight():
+grid = [None] * 10000
+pathOne = [1997,4543,2529,4916,1855,4705,2159,3444,1234,3639,2178,4682,2836,3333,1571,4906,2583,3872,2733,3815,2484,4641,1649,3378,226,366,2659,427,14,3325,2264,4711,2837,4986,238,3623,2830,4369,2469,4704,2302,3143,2771,3170,1237,3477,2251,4100,1561,4889,1857,4780,1258,4299,2975,4481,2692,4894,1847,4416,1670,4658,2537,3748,1468,4304,2263,4884,1806,413,1288,3933,14,3291,2809,4242,1669,450,1106,4510,1409,3311,1101,4232,1370,4490,2762,4805,2981,4637,2987,3403,1965,3724,2404,4664,2687,3868,2808,4174,2363,4241,254,4238,1444,375,1683,3712,2759,4569,1349,4378,2576,3437,1137,4822,121,4595,2602,3147,1959,3350,1964,3625,2718,3331,2252,4386,2251,3371,1973,4709,1915,4837,27,3727,2501,4520,2626,3161,2287,4224,2821,3555,2312,3234,2335,4572,2113,3673,2615,4919,1925,316,1211,377,1630,3786,1850,4221,1939,3559,1887,3779,2222,4482,2252,4682,2904,3568,1317,4453,1689,4917,1845,3260,169,3613,1528,4447,2791,4119,2268,3215,2806,3786,1465,4787,2792,4823,1526,4709,2362,4748,2518,3115,2898,3784,1893,3911,198,3215,1828,4100,1153,3496,2938,4403,1886,4317,2849,459,1156,427,264,4771,1956,3880,1313,4244,2483,417,172,3467,2475,4444,1554,4781,1524,4152,2771,3435,2622,4601,1733,4478,2686,412,2525,4467,2302,4948,2966,3572,2303,3914,154,4417,1635,4425,1640,4703,117,4187,2195,359,1166,4616,2557,3458,2743,4166,1328,4640,1908,4775,2151,4216,2964,4202,2534,4239,1998,3167,2604,4812,2527,3526,2640,393,2733,4980,1607,4879,2593,4721,1454,3137,1683,4343,238,4398,281,3392,1821,3247,2361,4208,1763,4771,2515]
+
+pathTwo = [21000,4189,2867,3824,2193,412,1704,383,1371,4858,2970,456,1877,4448,1962,3239,1641,4198,2840,4413,1586,4920,1650,3919,1375,4540,2150,3995,154,4200,161,4974,1249,3893,1319,3930,1658,3680,1286,4186,1963,3553,2256,3629,2554,3576,1887,3595,1629,4680,2684,3556,2302,3348,1825,4252,2684,3705,2258,472,1907,3702,2518,3440,1239,3258,1825,327,2580,4613,1357,4468,1519,3833,2415,4822,2798,3904,1812,376,186,3252,1427,3637,2896,3147,2294,3381,1306,3423,2688,4336,1648,3677,2750,4218,2649,4360,1710,464,1317,3232,1261,4167,249,4138,2431,4505,2535,3294,2553,3969,2144,3227,1437,4397,1359,3848,248,4992,1169,4580,2219,4525,1552,3546,1849,4722,1894,4735,2182,3570,1274,4349,1312,3430,1441,3183,1645,4308,2416,3333,2687,3202,2973,4736,1382,3260,2176,4207,1706,352,2142,4746,2328,4413,1879,4429,2679,4695,2224,4462,1358,4124,2515,4629,2873,4759,2763,328,1765,4426,293,3927,2395,3243,2393,4488,2729,3100,1488,483,147,392,2871,4410,1405,4993,1537,410,279,4218,2686,4563,231,3885,2784,4462,2160,3345,1204,3275,1162,3164,1843,4578,1255,4456,2398,3470,2576,4973,2337,4971,1205,3264,1707,3975,260,3270,11,3808,1844,4884,2952,4435,2144,4374,1389,4741,1404,4398,1282,4807,2316,3136,2504,3720,1859,4925,2711,3343,2535,4978,1578,3636,2447,4298,1574,3590,2142,4802,2846,4617,2838,3362,1812,3295,2328,3162,2617,4857,2759,4251,2343,3394,1721,3320,1836,3726,2950,4612,1129,3549,2970,487,2341,4269,2659,3550,1835,4318,2189,3278,1871,462,1703,3807,2389,3824,1521,4175,2698,3313,2942,4810,2498,318,1168,4111,1607]
+# To Prevent having to make a string, at the start of each number, 1 wi11 be right, 2 wi11 be left, 3 wi11 be up, and 4 wi11 be down.
+#Idea: Use the first digit to tell direction. Right will move positive by 1, Left will be negative by 1, Up will be postive by 1000, left will be negative by 1000
+startPoint = 4999
+def moveRight(line, grid, start, distance):
+    blocksNeeded = distance + 1
+    if line == 1:
+        for path in range(startPoint,blocksNeeded):
+            if grid[path] == None:
+                grid[path] = 1
+            elif grid[path] == 1:
+                grid[path] = 2
+            else:
+                print("error? at 16")
+    elif line == 2:
+        for path in range(startPoint,blocksNeeded):
+            if grid[path] == None:
+                grid[path] = 1
+            elif grid[path] == 1:
+                grid[path] = 3
+    return (start + blocksNeeded)
     pass
-def moveLeft():
+def moveLeft(line, grid,start, distance):
+    blocksNeeded = distance * -1 - 1
+    if line == 1:
+        for path in range(startPoint,blocksNeeded):
+            if grid[path] == None:
+                grid[path] = 1
+            elif grid[path] == 1:
+                grid[path] = 2
+            else:
+                print("error? at 16")
+    elif line == 2:
+        for path in range(startPoint,blocksNeeded):
+            if grid[path] == None:
+                grid[path] = 1
+            elif grid[path] == 1:
+                grid[path] = 3
+    return (start + blocksNeeded)
     pass
-def moveUp():
+def moveUp(line, grid, start, distance):
+    blocksNeeded = distance * -1000 - 1
+    if line == 1:
+        for path in range(startPoint,blocksNeeded):
+            if grid[path] == None:
+                grid[path] = 1
+            elif grid[path] == 1:
+                grid[path] = 2
+            else:
+                print("error? at 16")
+    elif line == 2:
+        for path in range(startPoint,blocksNeeded):
+            if grid[path] == None:
+                grid[path] = 1
+            elif grid[path] == 1:
+                grid[path] = 3
+    return (start + blocksNeeded)
     pass
-def moveDown():
+def moveDown(line, grid, start, distance):
+    blocksNeeded = distance * 1000 + 1
+    if line == 1:
+        for path in range(startPoint,blocksNeeded):
+            if grid[path] == None:
+                grid[path] = 1
+            elif grid[path] == 1:
+                grid[path] = 2
+            else:
+                print("error? at 16")
+    elif line == 2:
+        for path in range(startPoint,blocksNeeded):
+            if grid[path] == None:
+                grid[path] = 1
+            elif grid[path] == 1:
+                grid[path] = 3
+    return (start + blocksNeeded)
     pass
 def wireOne(pathData):
+    grid = [None] * 10000
+    startPoint = 4999
+    for move in pathData:
+        moveString = str(move)
+        fixedMove = moveString[1:]
+        if move == pathData[0]:
+            print("checking",move, pathData[0])
+            if moveString[0] == 1:
+                endPoint = moveRight(1, grid, startPoint, fixedMove)
+            elif moveString[0] == 2:
+                endPoint = moveLeft(1, grid, startPoint, fixedMove)
+            elif moveString[0] == 3:
+                endPoint = moveUp(1, grid, startPoint, fixedMove)
+            elif moveString[0] == 4:
+                endPoint = moveDown(1, grid, startPoint, fixedMove)
+        else:
+            if moveString[0] == 1:
+                endPoint = moveRight(1, grid, endPoint, fixedMove)
+            elif moveString[0] == 2:
+                endPoint = moveLeft(1, grid, endPoint, fixedMove)
+            elif moveString[0] == 3:
+                endPoint = moveUp(1, grid, endPoint, fixedMove)
+            elif moveString[0] == 4:
+                endPoint = moveDown(1, grid, endPoint, fixedMove)
+    return grid
+
+def wireTwo(grid, pathData):
     pass
-def wireTwo(pathData):
-    pass
+
+
+
+print(wireOne(pathOne))
